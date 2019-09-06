d7827085f620e150452edfbb0304bf7265224e4c
Andrew Nguyen <andrewdlqnguyen@gmail.com>
2019-09-06 17:34:53 -0400

    Made Aux wrapper and hoc folder

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-
+import Layout from './components/Layout/Layout';
 
 class App extends Component {
   render() {
     return (
       <div>
-        It's Me
+        <Layout>
+          <p>Testing</p>
+        </Layout>
       </div>
     );
   }
