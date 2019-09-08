a440ad9ace0cd2829285337ed18627fb19705c62
Unknown <andrewdlqnguyen@gmail.com>
2019-09-08 12:42:30 -0400

    Start of building the Burger App

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import burgerIngredient from './BurgerIngredient/BurgerIngredient';
+
+const Burger = () => {
+
+}
---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import Aux from '../../hoc/Auxiliary';
+
+class BurgerBuilder extends Component {
+    render() {
+        return (
+            <Aux>
+                <div>Burger View</div>
+                <div>Burger Build Controls</div>
+            </Aux>
+        );
+    }
+}
+
+export default BurgerBuilder;
