316b6516b557b32507cab4b83b5967b8ffbd51ae
Andrew Nguyen <andrewdlqnguyen@gmail.com>
2019-09-11 01:45:38 -0400

    Made  burger that changes dynamically and present on web

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,6 +1,22 @@
 import React from 'react';
-import burgerIngredient from './BurgerIngredient/BurgerIngredient';
+import classes from './Burger.css';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
-const Burger = () => {
+const burger = (props) => {
+    const transformedIngredients = Object.keys(props.ingredients)//array of the ingredient keys
+    .map(igKey => {
+        return [...Array(props.ingredients[igKey])].map((_, i) => {
+            return <BurgerIngredient key={igKey + i} type={igKey} />
+        })
+    }); 
 
-}
+    return (
+        <div className={classes.Burger}>
+            <BurgerIngredient type="bread-top"/>
+            {transformedIngredients}
+            <BurgerIngredient type="bread-bottom"/>
+        </div>
+    );
+};
+
+export default burger;
