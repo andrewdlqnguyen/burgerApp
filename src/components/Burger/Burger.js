34754cc6cc22293bc2ebdd1210eedb62c39922c2
Andrew Nguyen <andrewdlqnguyen@gmail.com>
2019-09-11 16:32:03 -0400

    Using map() and reduce() to dynamically change ingredients.

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,18 +3,25 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients)//array of the ingredient keys
-    .map(igKey => {
-        return [...Array(props.ingredients[igKey])].map((_, i) => {
-            return <BurgerIngredient key={igKey + i} type={igKey} />
+    let transformedIngredients = Object.keys(props.ingredients)//array of the ingredient keys
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <BurgerIngredient key={igKey + i} type={igKey} />
+            })
         })
-    }); 
-
+        .reduce((arr, el) => {
+            return arr.concat(el)
+        }, []); // we do this so to find out how many ingredients there are. Without this, the array will
+    // have a list of array objects. Instead, better to merge them together
+    console.log(transformedIngredients);
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients!</p>
+    }
     return (
         <div className={classes.Burger}>
-            <BurgerIngredient type="bread-top"/>
+            <BurgerIngredient type="bread-top" />
             {transformedIngredients}
-            <BurgerIngredient type="bread-bottom"/>
+            <BurgerIngredient type="bread-bottom" />
         </div>
     );
 };
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,12 +4,25 @@ import Aux from '../../hoc/Auxiliary';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //         ingredients: {
+    //             salad: 1,
+    //             bacon: 1,
+    //             cheese: 2,
+    //             meat: 2
+    //         }
+    //     }
+    // }
+
     state = {
+        //list of object, not an array (must transform into an array for Burger.js)
         ingredients: {
-            salad: 1,
-            bacon: 1,
-            cheese: 2,
-            meat: 2
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0
         }
     }
 
