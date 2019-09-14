d1e502367c7e0ceb84d8395e06844c639a7c440d
Andrew Nguyen <andrewdlqnguyen@gmail.com>
2019-09-14 16:52:54 -0400

    Rearranged Folder. Simple App Done.
    Look into HTTP and AJAX

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -4,14 +4,14 @@ import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './SideDrawer.css';
 import Backdrop from '../../UI/Backdrop/Backdrop';
-import Aux from '../../../hoc/Auxiliary';
+import Aux from '../../../hoc/Auxiliary/Auxiliary';
 
 const sideDrawer = (props) => {
     let attachedClasses = [classes.SideDrawer, classes.Close];
     if(props.open) {
         attachedClasses = [classes.SideDrawer, classes.Open];
     }
-    
+
     return (
         <Aux>
             <Backdrop show={props.open} clicked={props.closed}/>
