9533a815510144be6d1bbd34d875d6ad7c581d32
Andrew Nguyen <andrewdlqnguyen@gmail.com>
2019-09-13 22:32:22 -0400

    Implemented OrderSummary with Cancel and Continue Button

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -90,6 +90,10 @@ class BurgerBuilder extends Component {
         this.setState({ purchasing: false });
     }
 
+    purchaseContinueHandler = () => {
+        alert('You Continue');
+    }
+
     render() {
         const disabledInfo = {
             ...this.state.ingredients
@@ -102,7 +106,11 @@ class BurgerBuilder extends Component {
                 <Modal
                     show={this.state.purchasing}
                     modalClosed={this.purchaseCancelHandler}>
-                    <OrderSummary ingredients={this.state.ingredients} />
+                    <OrderSummary 
+                        price={this.state.totalPrice}
+                        purchaseCancelled={this.purchaseCancelHandler}
+                        purchaseContinued={this.purchaseContinueHandler}
+                        ingredients={this.state.ingredients} />
                 </Modal>
                 <Burger ingredients={this.state.ingredients} />
                 <BurgerControls
