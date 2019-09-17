1cbd3f4d52faddbd7e2247cb5229f27fadafcf54
Andrew Nguyen <andrewdlqnguyen@gmail.com>
2019-09-17 02:20:58 -0400

    Installed axios and post data to Firebase DB

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import Burger from '../../components/Burger/Burger';
 import BurgerControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -91,7 +92,24 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        alert('You Continue');
+        // alert('You Continue');
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer: {
+                name: 'Cindy',
+                andress: {
+                    street: 'Mavis Rd',
+                    zipCode: '010101',
+                    country: 'Canada'
+                },
+                email: 'princesscindy@hotmail.com'
+            },
+            deliveryMethod: 'express'
+        }
+        axios.post('/orders.json', order)
+            .then(response => console.log(response))
+            .catch(error => console.log(error));
     }
 
     render() {
